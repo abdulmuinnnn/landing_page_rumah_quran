6e1d400841943cc06247f9fff085aae0106e627e
abdulmuinn <muinabdul000@gmail.com>
2025-10-31 08:38:52 +0700

    Fix vite base path and build config for Netlify

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
+// ✅ konfigurasi fix untuk Netlify
 export default defineConfig({
+  plugins: [react()],
+  base: './', // <--- tambahkan ini
+})
+
+
+
+// https://vitejs.dev/config/
+/*export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
 });
+*/
